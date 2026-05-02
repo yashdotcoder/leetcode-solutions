74828f0071ca42fc51095444a800f86edfaed0b4
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-05-02 18:18:53 +0530

    0002-add-two-numbers Linked List

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,36 +10,33 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null;
+        ListNode head = new ListNode(0);
         ListNode curr = head;
         int carry = 0;
 
         while (l1 != null || l2 != null) {
-            int n1 = l1 == null ? 0 : l1.val;
-            int n2 = l2 == null ? 0 : l2.val;
-            int res = n1 + n2 + carry;
+            int n1 = (l1 == null) ? 0 : l1.val;
+            int n2 = (l2 == null) ? 0 : l2.val;
+            int sum = n1 + n2 + carry;
 
-            carry = res / 10;
-            int units = res % 10;
+            carry = sum / 10;
 
-            ListNode next = new ListNode(units);
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
 
-            if (head == null) {
-                head = new ListNode(units);
-                curr = head;
-            } else {
-                curr.next = next;
-                curr = next;
+            if (l1 != null) {
+                l1 = l1.next;
             }
 
-            l1 = l1 == null ? null : l1.next;
-            l2 = l2 == null ? null : l2.next;
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
 
         if (carry != 0) {
             curr.next = new ListNode(carry);
         }
 
-        return head;
+        return head.next;
     }
 }
