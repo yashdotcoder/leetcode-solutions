de9b08581c8f774206cd7c6b031958040fb4edb5
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-05-05 20:27:18 +0530

    0347-top-k-frequent-elements Array

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -7,6 +7,26 @@ class Solution {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
         
+        // Step 2: Min Heap based on frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
+            new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
 
+        // Step 3: Keep only top k elements in heap
+        for (Map.Entry<Integer, Integer> entry: freqMap.entrySet()) {
+            minHeap.offer(entry);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        // Step 4: Build result array
+        int [] result = new int[k];
+
+        for (int i = 0; i < k; ++i) {
+            result[i] = minHeap.poll().getKey();
+        }
+
+        return result;
     }
 }
