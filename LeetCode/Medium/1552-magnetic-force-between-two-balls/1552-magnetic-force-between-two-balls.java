10b77b7ecd7a5bc741fbe6f7c70a2c9321d5d5f4
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-06-11 10:08:51 +0530

    1552-magnetic-force-between-two-balls Array

--- LeetCode/Medium/1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ LeetCode/Medium/1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,23 +1,33 @@
 class Solution {
 
-    boolean canPlaceBalls(int[] position, int distance, int m) {
-        int placed = 1;
+    // Check if we can place 'm' balls at 'position'
+    // with each ball having at least 'distance' gap.
+    private boolean canPlaceBalls(int[] position, int distance, int m) {
+        // Place the first ball at first position
+        int ballsPlaced = 1;
         int prevPos = position[0];
 
+        // Iterate on each position and place a ball there is we can place it.
         for (int i = 1; i < position.length; ++i) {
-            if (position[i] - prevPos >= distance) {
-                placed++;
+            int currPos = position[i];
+
+            if (currPos - prevPos >= distance) {
+                // Increment the ballsPlaced and update the prevPos
+                ballsPlaced++;
                 prevPos = position[i];
             }
         }
 
-        return placed >= m;
+        // If we can place balls greater than required balls
+        return ballsPlaced >= m;
     }
 
 
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
         int N = position.length;
+
+        // Initial search space
         int low = 1;
         int high = (int) Math.ceil((position[N - 1] - position[0]) * 1.0 / (m - 1));
         int ans = 0;
@@ -25,10 +35,16 @@ class Solution {
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
+            // If we can place all balls having a gap at least 'mid'
             if (canPlaceBalls(position, mid, m)) {
+                // mid is our answer
                 ans = mid;
+
+                // discard the left search space
                 low = mid + 1;
             } else {
+
+                // discard the right half search space
                 high = mid - 1;
             }
         }
