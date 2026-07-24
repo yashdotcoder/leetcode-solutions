893c7a74e6c1d4dcc8581a4acf6556efb0ea921a
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-07-24 22:37:48 +0530

    0046-permutations Array

--- LeetCode/Medium/0046-permutations/0046-permutations.java
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -1,11 +1,34 @@
 class Solution {
+    private List<List<Integer>> permutations = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int[] freq = new int[nums.length];
-        recurse(nums, freq, new ArrayList<>(), ans);
-        return ans;
+        
+        recurse(nums, 0);
+
+        return permutations;
     }
 
+    private void recurse(int[] nums, int start) {
+        if (start == nums.length) {
+            permutations.add(Arrays.stream(nums).boxed().toList());
+            return;
+        }
+
+        for (int i = start; i < nums.length; ++i) {
+            swap(nums, i, start);
+            recurse(nums, start + 1);
+            swap(nums, i, start);
+        }
+    }
+
+    private void swap(int[] nums, int idx1, int idx2) {
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+
+
+    /*
     private void recurse(int[] nums, int[] freq, List<Integer> collection, List<List<Integer>> ans) {
         if (collection.size() == nums.length) {
             ans.add(new ArrayList<>(collection));
@@ -22,5 +45,5 @@ class Solution {
             }
         }
     }
-    
+    */
 }
