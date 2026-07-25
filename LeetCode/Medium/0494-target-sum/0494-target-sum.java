6cf59966ce3db0ce8c91c7f49bbc65894692b492
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-07-25 21:12:13 +0530

    0494-target-sum Array

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,18 +1,35 @@
 class Solution {
+
+    int totalSum;
+    
     public int findTargetSumWays(int[] nums, int target) {
-        return count(nums, target, 0);
+        totalSum = Arrays.stream(nums).sum();
+
+        int[][] memo = new int[nums.length][2 * totalSum + 1];
+
+        for (int i = 0; i < nums.length; ++i) {
+            Arrays.fill(memo[i], Integer.MIN_VALUE);
+        }
+
+        return calculateWays(nums, target, 0, 0, memo);
     }
 
-    private int count(int[] nums, int target, int index) {
+    private int calculateWays(int[] nums, int target, int index, int sum, int[][] memo) {
         if (index == nums.length) {
-            if (target == 0) {
+            if (sum == target) {
                 return 1;
             }
             return 0;
         }
 
-        return count(nums, target - nums[index], index + 1) 
-                + count(nums, target + nums[index], index + 1);
+        if (memo[index][sum + totalSum] != Integer.MIN_VALUE) {
+            return memo[index][sum + totalSum];
+        }
 
+        int add =  calculateWays(nums, target , index + 1, sum - nums[index], memo);
+        
+        int subtract = calculateWays(nums, target, index + 1, sum + nums[index], memo);
+
+        return memo[index][sum + totalSum] = add + subtract;
     } 
 }
