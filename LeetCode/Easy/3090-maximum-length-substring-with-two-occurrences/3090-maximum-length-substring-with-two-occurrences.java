dd18de4c130018d9cc70ffc482c3f5d810b8bc32
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-08-14 21:06:07 +0530

    3090-maximum-length-substring-with-two-occurrences Hash Table

--- LeetCode/Easy/3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ LeetCode/Easy/3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -2,20 +2,21 @@ class Solution {
     public int maximumLengthSubstring(String s) {
         int res = 1;
         int n = s.length();
+        int left = 0;
+        int[] count = new int[26];
 
-        for (int left = 0; left < n; ++left) {
-            int[] count = new int[26];
+        for (int right = left; right < n; ++right) {
+            int ch = s.charAt(right) - 'a';
 
-            for (int right = left; right < n; ++right) {
-                int ch = s.charAt(right) - 'a';
-                count[ch]++;
+            count[ch]++;
 
-                if (count[ch] <= 2) {
-                    res = Math.max(res, right - left + 1);
-                } else {
-                    break;
-                }
+            while (count[ch] > 2) {
+                int chLeft = s.charAt(left) - 'a';
+                count[chLeft]--;
+                left++;
             }
+
+            res = Math.max(res, right - left + 1);
         }
 
         return res;
