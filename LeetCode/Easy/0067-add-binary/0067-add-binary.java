2e7cfafc9a157d35397f209894fe46a4a1675881
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-09-24 10:13:56 +0530

    0067-add-binary Math

--- LeetCode/Easy/0067-add-binary/0067-add-binary.java
+++ LeetCode/Easy/0067-add-binary/0067-add-binary.java
@@ -1,47 +1,20 @@
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-        if (b.length() > a.length()) {
-            return addBinary(b, a);
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger carry, ans;
+        BigInteger zero = new BigInteger("0", 2);
+
+        while (y.compareTo(zero) != 0) {
+            ans = x.xor(y);
+            carry = x.and(y).shiftLeft(1);
+
+            x = ans;
+            y = carry;
         }
 
-        int carry = 0;
-        int indexA = a.length() - 1;
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = b.length() - 1; i >= 0; --i, indexA--) {
-            int digitB = b.charAt(i) - '0';
-            int digitA = a.charAt(indexA) - '0';
-
-            int digitSb = digitA + digitB + carry;
-
-            if (digitSb >= 2) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-
-            sb.append(digitSb % 2);
-        }
-
-        // Append remaining digits of a
-         while (indexA >= 0) {
-            int digitA = a.charAt(indexA) - '0';
-            int digitSb = digitA + carry;
-
-            if (digitSb >= 2) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-
-            sb.append(digitSb % 2);
-            indexA--;
-        }
-
-        if (carry != 0) {
-            sb.append(carry);
-        }
-
-        return sb.reverse().toString();
+        return x.toString(2);
     }
 }
