81f68aceacdfe171566ba47656c1b0d57074a4de
Yash Singh <42043341+yashdotcoder@users.noreply.github.com>
2026-09-24 09:18:10 +0530

    3550-smallest-index-with-digit-sum-equal-to-index Array

--- LeetCode/Easy/3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ LeetCode/Easy/3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,25 +1,21 @@
 class Solution {
+    static int[] precomp;
+    static int MAX_VAL = 1000;
+    static {
+        precomp = new int[MAX_VAL + 1];
+        for (int i = 0; i <= MAX_VAL; ++i) {
+            precomp[i] = precomp[i / 10] + (i % 10);
+        }
+    }
     public int smallestIndex(int[] nums) {
         for (int i = 0; i < nums.length; ++i) {
             int num = nums[i];
-            int sumOfDigits = findSumOfDigits(num);
 
-            if (sumOfDigits == i) {
+            if (precomp[num] == i) {
                 return i;
             }
         }
 
         return -1;
-    }
-
-    private int findSumOfDigits(int num) {
-        int sum = 0;
-
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-
-        return sum;
     }
 }
